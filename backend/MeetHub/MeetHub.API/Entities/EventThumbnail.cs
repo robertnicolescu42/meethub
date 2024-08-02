e17bcd350b515b44b3fa3da0fbf57bf1444c6b33
Remus Ene <remusene69@gmail.com>
2024-08-02 21:06:52 +0300

    Added event thumbnail entity

---
+++ backend/MeetHub/MeetHub.API/Entities/EventThumbnail.cs
@@ -0,0 +1,35 @@
+﻿using MeetHub.API.Helpers;
+using System.ComponentModel.DataAnnotations;
+
+namespace MeetHub.API.Entities
+{
+    /// <summary>
+    /// The event thumbnail entity class
+    /// </summary>
+    public class EventThumbnail
+    {
+        #region Properties
+
+        /// <summary>
+        /// Gets or sets the Id field of the event thumbnail class
+        /// </summary>
+        [Key]
+        public int Id
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// Gets or sets the source field of the event thumbnail class 
+        /// </summary>
+        [Required(ErrorMessage = Constants.cmRequiredErrorMessage)]
+        public string Source 
+        { 
+            get; 
+            set; 
+        }
+
+        #endregion Properties
+    }
+}
