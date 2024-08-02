8109f820d5805c034bd0b872e1d3f4a17124db83
Remus Ene <remusene69@gmail.com>
2024-08-03 02:25:03 +0300

    Added UserEventRelation Entity

---
+++ backend/MeetHub/MeetHub.API/Entities/UserEventRelation.cs
@@ -0,0 +1,53 @@
+﻿using MeetHub.API.Helpers;
+using System.ComponentModel.DataAnnotations;
+
+namespace MeetHub.API.Entities
+{
+    /// <summary>
+    /// The user-event relation entity class
+    /// </summary>
+    public class UserEventRelation
+    {
+        #region Properties
+
+        /// <summary>
+        /// Gets or sets the user Id field of user-event relation class
+        /// </summary>
+        [Required(ErrorMessage = Constants.cmRequiredErrorMessage)]
+        public int UserId
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// Gets or sets the event Id of the user-event relation class
+        /// </summary>
+        [Required(ErrorMessage = Constants.cmRequiredErrorMessage)]
+        public int EventId
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// Gets or sets the user navigation property of the user-event relation class
+        /// </summary>
+        public User User
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// Gets or sets the event navigation property of the user-event relation class
+        /// </summary>
+        public Event Event
+        {
+            get;
+            set;
+        }
+
+        #endregion Properties
+    }
+}
