0779c6c6dda26cff109fd76825e5a907eba64d61
Remus Ene <remusene69@gmail.com>
2024-08-02 20:53:17 +0300

    Moved constant field to Constant class

--- backend/MeetHub/MeetHub.API/Entities/Location.cs
+++ backend/MeetHub/MeetHub.API/Entities/Location.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using MeetHub.API.Helpers;
+using System.ComponentModel.DataAnnotations;
 
 namespace MeetHub.API.Entities
 {
@@ -7,13 +8,6 @@ namespace MeetHub.API.Entities
     /// </summary>
     public class Location
     {
-
-        #region Fields
-
-        private const string cmRequiredErrorMessage = "This field is a required field";
-
-        #endregion Fields
-
         #region Properties
 
         /// <summary>
@@ -29,7 +23,7 @@ namespace MeetHub.API.Entities
         /// <summary>
         /// The latitude field of the location class
         /// </summary>
-        [Required(ErrorMessage = cmRequiredErrorMessage)]
+        [Required(ErrorMessage = Constants.cmRequiredErrorMessage)]
         public string Latitude
         {
             get;
@@ -39,7 +33,7 @@ namespace MeetHub.API.Entities
         /// <summary>
         /// The longitude field of the location class
         /// </summary>
-        [Required(ErrorMessage = cmRequiredErrorMessage)]
+        [Required(ErrorMessage = Constants.cmRequiredErrorMessage)]
         public string Longitude
         {
             get;
