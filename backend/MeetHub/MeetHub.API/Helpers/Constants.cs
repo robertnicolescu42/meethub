6d3a4f43c98e8ea265cbcbf0c13040a70a29dc1c
Remus Ene <remusene69@gmail.com>
2024-08-02 20:52:52 +0300

    Created constant class

---
+++ backend/MeetHub/MeetHub.API/Helpers/Constants.cs
@@ -0,0 +1,14 @@
+﻿namespace MeetHub.API.Helpers
+{
+    /// <summary>
+    /// The constants class. All common constant fields used inside MHB.API project
+    /// </summary>
+    public static class Constants
+    {
+        #region Fields
+
+        public const string cmRequiredErrorMessage = "This field is a required field";
+
+        #endregion Fields
+    }
+}
