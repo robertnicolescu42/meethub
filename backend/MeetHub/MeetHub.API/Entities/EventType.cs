2a0d2d5b4070b71e96d53ac8cf567f9fc9a49205
Remus Ene <remusene69@gmail.com>
2024-08-02 20:53:30 +0300

    Added event type entity

---
+++ backend/MeetHub/MeetHub.API/Entities/EventType.cs
@@ -0,0 +1,44 @@
+﻿using MeetHub.API.Helpers;
+using System.ComponentModel.DataAnnotations;
+
+namespace MeetHub.API.Entities
+{
+    /// <summary>
+    /// The event type entity class
+    /// </summary>
+    public class EventType
+    {
+        #region Properties
+
+        /// <summary>
+        /// The Id field of the event type class
+        /// </summary>
+        [Key]
+        public int Id
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// The title field of the eevent type class
+        /// </summary>
+        [Required(ErrorMessage = Constants.cmRequiredErrorMessage)]
+        public string Title
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// The description field of the event type class
+        /// </summary>
+        public string Description
+        {
+            get;
+            set;
+        }
+
+        #endregion Properties
+    }
+}
