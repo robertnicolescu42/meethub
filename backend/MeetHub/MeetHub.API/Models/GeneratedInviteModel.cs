31cf57bf174da282bb39fa5333dd685f1b433fed
Remus Ene <remusene69@gmail.com>
2024-08-03 18:14:54 +0300

    Added generated invite model

---
+++ backend/MeetHub/MeetHub.API/Models/GeneratedInviteModel.cs
@@ -0,0 +1,75 @@
+﻿namespace MeetHub.API.Models
+{
+    /// <summary>
+    /// The generated invite model class
+    /// </summary>
+    public class GeneratedInviteModel
+    {
+        #region Properties
+
+        /// <summary>
+        /// Gets or sets the Id field of the generated invite class
+        /// </summary>
+        public int Id
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// Gets or sets the user Id field of the generated invite class
+        /// </summary>
+        public int UserId
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// Gets or sets the event Id field of the generated invite class
+        /// </summary>
+        public int NumberOfUses
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// Gets or sets the invite code field of the generated invide class
+        /// </summary>
+        public string InviteCode
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// Gets or sets the number of uses field of the generated invite class
+        /// </summary>
+        public int EventId
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// Gets or sets the crreated at field of the generated invite class
+        /// </summary>
+        public DateTime CreatedAt
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// Gets or sets the expire on field of the generated invite class
+        /// </summary>
+        public DateTime ExpireOn
+        {
+            get;
+            set;
+        }
+
+        #endregion Properties
+    }
+}
