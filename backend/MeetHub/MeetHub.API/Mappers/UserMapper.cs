4c84ee2eb137b405e901789ca2b26859f43b81a7
Remus Ene <remusene69@gmail.com>
2024-08-09 20:57:47 +0300

    Added mapper for user

---
+++ backend/MeetHub/MeetHub.API/Mappers/UserMapper.cs
@@ -0,0 +1,24 @@
+﻿using AutoMapper;
+using MeetHub.API.Entities;
+using MeetHub.API.Models;
+
+namespace MeetHub.API.Mappers
+{
+    /// <summary>
+    /// The mapper between user entity and model
+    /// </summary>
+    public class UserMapper : Profile
+    {
+        #region Constructors
+        
+        /// <summary>
+        /// The constructor of the mapper class
+        /// </summary>
+        public UserMapper()
+        {
+            CreateMap<User, UserModel>().ReverseMap();
+        }
+
+        #endregion Constructors
+    }
+}
