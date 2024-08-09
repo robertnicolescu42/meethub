44df40885b7e44f2e6ac30599772851fb22a6253
Remus Ene <remusene69@gmail.com>
2024-08-09 20:58:03 +0300

    Added mapper for user event relation

---
+++ backend/MeetHub/MeetHub.API/Mappers/UserEventRelationMapper.cs
@@ -0,0 +1,25 @@
+﻿using AutoMapper;
+using MeetHub.API.Entities;
+using MeetHub.API.Models;
+
+namespace MeetHub.API.Mappers
+{
+    /// <summary>
+    /// The mapper between user-event relation entity and model
+    /// </summary>
+    public class UserEventRelationMapper : Profile
+    {
+        #region Constructors
+
+        /// <summary>
+        /// The constructor of the mapper class
+        /// </summary>
+        public UserEventRelationMapper()
+        {
+            CreateMap<UserEventRelation, UserEventRelationModel>().ReverseMap();
+        }
+
+        #endregion Constructors
+
+    }
+}
