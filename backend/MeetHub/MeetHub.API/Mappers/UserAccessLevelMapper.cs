285544cbd8cfe436408a7d86bed65718527be919
Remus Ene <remusene69@gmail.com>
2024-08-09 20:58:17 +0300

    Added mapper for user access level

---
+++ backend/MeetHub/MeetHub.API/Mappers/UserAccessLevelMapper.cs
@@ -0,0 +1,26 @@
+﻿using AutoMapper;
+using MeetHub.API.Entities;
+using MeetHub.API.Models;
+
+namespace MeetHub.API.Mappers
+{
+    /// <summary>
+    /// The mapper between user access level entity and model
+    /// </summary>
+
+    public class UserAccessLevelMapper : Profile
+    {
+        #region Constructors
+
+        /// <summary>
+        /// The constructor of the mapper class
+        /// </summary>
+        public UserAccessLevelMapper()
+        {
+            CreateMap<UserAccessLevel, UserAccessLevelModel>().ReverseMap();
+        }
+
+        #endregion Constructors
+
+    }
+}
