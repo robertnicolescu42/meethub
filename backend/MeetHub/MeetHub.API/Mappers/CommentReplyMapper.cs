5cc51085560a8e62711534909f2683c163a73384
Remus Ene <remusene69@gmail.com>
2024-08-09 20:59:44 +0300

    Added mapper for comment reply

---
+++ backend/MeetHub/MeetHub.API/Mappers/CommentReplyMapper.cs
@@ -0,0 +1,25 @@
+﻿using AutoMapper;
+using MeetHub.API.Entities;
+using MeetHub.API.Models;
+
+namespace MeetHub.API.Mappers
+{
+    /// <summary>
+    /// The mapper between comment reply entity and model
+    /// </summary>
+    public class CommentReplyMapper : Profile
+    {
+        #region Constructors
+
+        /// <summary>
+        /// The constructor of the mapper class
+        /// </summary>
+        public CommentReplyMapper()
+        {
+            CreateMap<CommentReply, CommentReplyModel>().ReverseMap();
+        }
+
+        #endregion Constructors
+
+    }
+}
