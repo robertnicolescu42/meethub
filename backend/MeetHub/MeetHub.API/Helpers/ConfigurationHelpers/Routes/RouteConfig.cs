df5c2e120d7898a206abaf6588752e82269f580d
Remus Ene <remusene69@gmail.com>
2024-08-17 22:47:06 +0300

    Added route configuration model

---
+++ backend/MeetHub/MeetHub.API/Helpers/ConfigurationHelpers/Routes/RouteConfig.cs
@@ -0,0 +1,39 @@
+﻿namespace MeetHub.API.Helpers.ConfigurationHelpers.Routes
+{
+    /// <summary>
+    /// The route configuration class
+    /// </summary>
+    public class RouteConfig
+    {
+        #region Properties
+
+        /// <summary>
+        /// The template of the route
+        /// </summary>
+        public string Template 
+        { 
+            get; 
+            set; 
+        }
+
+        /// <summary>
+        /// The controller name
+        /// </summary>
+        public string Controller 
+        { 
+            get; 
+            set; 
+        }
+
+        /// <summary>
+        /// The method name
+        /// </summary>
+        public string Action 
+        { 
+            get; 
+            set; 
+        }
+
+        #endregion Properties
+    }
+}
