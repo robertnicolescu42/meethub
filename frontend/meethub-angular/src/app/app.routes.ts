54df5be224791cbf01831cfe0a4886bf3f212bd5
Robert Nicolescu <robertnicolescu42@yahoo.com>
2024-09-30 14:15:51 +0300

    MHB-91 - Initialized public routes

--- frontend/meethub-angular/src/app/app.routes.ts
+++ frontend/meethub-angular/src/app/app.routes.ts
@@ -1,3 +1,27 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { LoginComponent } from './public/login/login.component';
+import { SignupComponent } from './public/signup/signup.component';
+import { AboutComponent } from './public/about/about.component';
+
+export const routes: Routes = [
+  // Public routes
+  {
+    path: 'public',
+    children: [
+      { path: 'login', component: LoginComponent },
+      { path: 'signup', component: SignupComponent },
+      { path: 'logout', redirectTo: 'login' },
+      { path: 'about', component: AboutComponent },
+    ],
+  },
+
+  // Secure routes
+  {
+    path: 'secure',
+    children: [],
+  },
+
+  // Fallback route
+  { path: '**', redirectTo: 'public/login', pathMatch: 'full' },
+];
