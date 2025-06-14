3ef92cf2642a1610ee3191b9b0b8595ba59853b9
Remus Ene <remusene69@gmail.com>
2025-06-14 22:48:31 +0300

    Changes

--- backend/MeetHub/MeetHub.API/Program.cs
+++ backend/MeetHub/MeetHub.API/Program.cs
@@ -62,27 +62,13 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-app.UseHttpsRedirection();
+//app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
 #region Routes mapping
 
-var routeConfigs = RouteConfigRetriever.GetFileRoutes();
-
 app.UseRouting();
-
-app.UseEndpoints(endpoints =>
-{
-    foreach (var routeConfig in routeConfigs)
-    {
-        endpoints.MapControllerRoute(
-            name: $"{routeConfig.Controller}_{routeConfig.Action}",
-            pattern: routeConfig.Template,
-            defaults: new { controller = routeConfig.Controller, action = routeConfig.Action }
-        );
-    }
-});
 
 app.MapControllers();
 
